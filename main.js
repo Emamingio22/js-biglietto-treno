dc01628f47f1835e3dcd0eb9ff4cdd332d518c5e
emamingio22 <emawankenoby02@gmail.com>
2023-03-29 15:32:03 +0200

    I made the ticket price and the discount

--- main.js
+++ main.js
@@ -7,16 +7,28 @@ va applicato uno sconto del 40% per gli over 65.*/
 
 
 /*numero km ed età passeggero*/
-let kmNumber = prompt("Di quanti km sarà il tuo viaggio?");
-console.log(kmNumber)
-let age = prompt("Quanti anni hai?");
-console.log(age)
+    let kmNumber = prompt("Di quanti km sarà il tuo viaggio?");
+    console.log(kmNumber)
+    let age = prompt("Quanti anni hai?");
+    console.log(age)
 
 /*calcolo prezzo standard*/
-let standardPrice = kmNumber * 0.21;
-console.log(standardPrice)
+    let standardPrice = kmNumber * 0.21;
+    console.log(standardPrice)
 
 
 
+/*calcolo prezzo per gli Under 18 e gli Over 65*/
+    if (age < 18) {
+        let underDiscount = ((standardPrice * 20) / 100);
+        let underCost = standardPrice - underDiscount;
+        console.log(underCost);
+    }else if (age > 65) {
+        let overDiscount = ((standardPrice * 40) / 100);
+        let overCost = standardPrice - overDiscount;
+        console.log(overCost);
+    }else {
+        console.log("non è stato applicato nessuno sconto");
+    }
 
 
